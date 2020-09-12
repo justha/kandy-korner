ce8a3caa8eb002291d671c42af6becdd68d8a554
justha <nganha42@gmail.com>
2020-09-12 14:22:07 -0500

    initial commit

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./KandyKorner.css"
+
+export const KandyKorner = () => (
+    <>
+        <h1>Kandy Korner</h1>
+        <small>your neighborhood kandy store</small>
+    </>
+)
